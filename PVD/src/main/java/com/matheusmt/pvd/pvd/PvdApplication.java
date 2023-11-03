a0b393a7bca052ea990cc837f22326463a81c8a4
matheus <matheusdacruzsouza1998@gmail.com>
2023-11-03 10:25:09 -0300

    teste

--- PVD/src/main/java/com/matheusmt/pvd/pvd/PvdApplication.java
+++ PVD/src/main/java/com/matheusmt/pvd/pvd/PvdApplication.java
@@ -1,13 +1,13 @@
 package com.matheusmt.pvd.pvd;
 
+import com.matheusmt.pvd.pvd.entity.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping()
+
 @SpringBootApplication
 public class PvdApplication {
 
@@ -16,9 +16,10 @@ public class PvdApplication {
     }
 
 
-    @GetMapping("/teste")
-    public String teste(){
-        return "hello word";
+    public void teste(){
+        User u = new User();
+
+
     }
 
 
